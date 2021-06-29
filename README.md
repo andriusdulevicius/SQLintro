28650dd3a1026115addb25ba2d11b0a9390a4dcd
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-29 09:59:41 +0300

    creating db , connecting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SQLintro
+# Simple mysql express app
+
+1.npm i mysql express morgan
+
+2. isitraukiam viska i app.js
+
+3.
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('hello sql');
+const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
+
+//set up connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+});
+
+//connect
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Express veikia normaliai');
+});
+
+app.get('/createdb', (req, res) => {
+  const sql = 'CREATE TABLE nodeSql';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    //no errors
+    console.log(result);
+  });
+});
+
+app.listen('3000', console.log('server running on port 3000'));
