506d76a82f696664f8b09cd90981d8e9f0b2c1fc
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-29 11:32:09 +0300

    get request from sql

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
+  database: 'nodeSqlJS3',
 });
 
 //connect
@@ -23,11 +24,60 @@ app.get('/', (req, res) => {
 });
 
 app.get('/createdb', (req, res) => {
-  const sql = 'CREATE TABLE nodeSql';
+  const sql = 'CREATE DATABASE nodeSqlJS3';
   db.query(sql, (err, result) => {
     if (err) throw err;
     //no errors
     console.log(result);
+    res.send('duomenu baze sukurta');
+  });
+});
+
+//create table
+app.get('/table/create', (req, res) => {
+  const sql = `
+  CREATE TABLE posts(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      title VARCHAR(255) NOT NULL,
+      body TEXT NOT NULL,
+      created_at TIMESTAMP on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (id)
+  )`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json({ result, msg: 'lentele sukurta' });
+  });
+});
+
+//add new post
+app.get('/new/post', (req, res) => {
+  const newPost = { title: 'Third post title', body: 'Third sql body sent into table' };
+  const sql = 'INSERT INTO posts SET ?';
+  db.query(sql, newPost, (err, result) => {
+    if (err) throw err.stack;
+    console.log(result);
+    res.json({ result, msg: 'post created' });
+  });
+});
+
+//get all posts
+
+app.get('/allposts', (req, res) => {
+  const sql = 'SELECT * FROM posts';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get('/post/:id', (req, res) => {
+  const sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
   });
 });
 
